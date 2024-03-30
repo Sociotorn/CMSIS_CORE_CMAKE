de54ecb747b0c3db54944661eee7bff331cfcfbb
sociotorn <sociotorn@gmail.com>
2024-03-29 23:11:09 -0500

    Add CMake files and test files

---
+++ tests/helpCompile.h
@@ -0,0 +1,7 @@
+#ifndef HELPCOMPILE_H
+#define HELPCOMPILE_H
+
+typedef int IRQn_Type;
+#define __NVIC_PRIO_BITS 0
+
+#endif
---
+++ tests/testcm7.c
@@ -0,0 +1,8 @@
+#include "helpCompile.h"
+#include "core_cm7.h"
+
+// This file just tests that CMSIS for m7 builds
+int main()
+{
+    return 0;
+}
